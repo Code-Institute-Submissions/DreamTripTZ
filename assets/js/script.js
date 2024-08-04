863da1a7c2bad4b19bc7e74bf592d8daf94a4cdd
D0bledore <verime@duck.com>
2024-08-04 13:20:28 +0200

    Add init function and initialize image element with Tanzania flag

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 // Variables
+const img = document.getElementById('img');
 
 // Methods
+function init() {
+    img.src = "assets/images/flag-tanzania.jpg";
+    img.alt = "Tanzania flag grunge"
+}
 
 // Inits &  Event Listeners
+init(); 
